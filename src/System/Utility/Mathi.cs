da08997b340c13bf1b02e42ebf442139a2ca93d3
William-McGonagle <mcgonaglew@gfacademy.org>
2021-06-22 18:17:01 -0600

    📝 Added Documentation for Mathi

--- src/System/Utility/Mathi.cs
+++ src/System/Utility/Mathi.cs
@@ -3,6 +3,13 @@ namespace BigTony.Utility
     public static class Mathi
     {
 
+        /// <summary>
+        /// Clamp an integer between a maximum and minimum amount.
+        /// </summary>
+        /// <param name="val">The value of the integer that needs to be clamped.</param>
+        /// <param name="min">The minimum amount that 'val' can be, as an integer.</param>
+        /// <param name="max">The maximum amount that 'val' can be, as an integer.</param>
+        /// <returns>A clamped value of 'val' between 'min' and 'max'.</returns>
         public static float Clamp(int val, int min, int max)
         {
 
@@ -12,6 +19,12 @@ namespace BigTony.Utility
 
         }
 
+        /// <summary>
+        /// Get the highest value between two inputted integers.
+        /// </summary>
+        /// <param name="a">The first integer to be compared.</param>
+        /// <param name="b">The second integer to be compared.</param>
+        /// <returns>The highest value between 'a' and 'b'.</returns>
         public static int Max(int a, int b)
         {
 
@@ -19,6 +32,12 @@ namespace BigTony.Utility
 
         }
 
+        /// <summary>
+        /// Get the lowest value between two inputted integers.
+        /// </summary>
+        /// <param name="a">The first integer number to be compared.</param>
+        /// <param name="b">The second integer number to be compared.</param>
+        /// <returns>The lowest value between 'a' and 'b'.</returns>  
         public static int Min(int a, int b)
         {
 
@@ -26,6 +45,11 @@ namespace BigTony.Utility
 
         }
 
+        /// <summary>
+        /// Get the absolute value of the inputted integer.
+        /// </summary>
+        /// <param name="a">The integer to get the absolute value of.</param>
+        /// <returns>The absolute value of 'a'.</returns>
         public static int Abs(int a)
         {
 
